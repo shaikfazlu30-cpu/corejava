fada39614e0a00dea649132bee28bf206ab739b1
shaikfazlu30-cpu <shaikfazlu30@gmail.com>
2025-11-19 11:33:07 +0530

    varibles pratice

--- src/net/konic/corejava/LoopsWork.java
+++ src/net/konic/corejava/LoopsWork.java
@@ -10,7 +10,7 @@ public class LoopsWork {
 		 if (a>b && a>c) {
 			 System.out.println("a is greater");
 		 } else if(b>a && b>c) {
-			 System.out.println("b is greater");
+			 System.out.println("b is greater"); 
 			 
 		 }else if (c>a && c>b) {
 			 System.out.println("c is greater");
